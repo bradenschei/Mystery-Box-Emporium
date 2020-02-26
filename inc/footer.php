eac967e2270b90672163a379381361789de89e42
Braden Scheidecker <53822929+bradenschei@users.noreply.github.com>
2020-02-26 12:41:02 -0600

    .

---
+++ inc/footer.php
@@ -0,0 +1,20 @@
+<div id="contact" class="offset">
+
+    <footer>
+        <div class="row justify-content-center">
+
+            <div class="col-md-5 text-center">
+                <img src="img/Logo/logo_transparent.png">
+                <p>REEEEEEEEEEEEEEEEEEEEEE</p>
+                <strong>Contact Info</strong>
+                <p>(888) 888-888<br>blank@gmail.com</p>
+                <a href="" target="_blank"><i class="fab fa-facebook-square"></i></a>
+                <a href="" target="_blank"><i class="fab fa-twitter-square"></i></a>
+                <a href="" target="_blank"><i class="fab fa-instagram"></i></a>
+            </div>
+
+            <hr class="socket">
+            <p>&copy; 2020 Mystery Inc.</p>
+    </footer>
+
+</div>
