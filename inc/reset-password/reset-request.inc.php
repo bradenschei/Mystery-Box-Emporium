bb1d0a36ee350c800c5183cac75ad75728214f06
Braden Scheidecker <53822929+bradenschei@users.noreply.github.com>
2020-03-15 01:58:35 -0500

    Update reset-request.inc.php

--- inc/reset-password/reset-request.inc.php
+++ inc/reset-password/reset-request.inc.php
@@ -51,12 +51,12 @@ if (isset($_POST['reset-request-submit'])) {
     $message .= '<a href="' . $url .'">' . $url . '</a></p>';
     $message .= '<br><br><p>Contact us here: mysteryboxemporium420@gmail.com</p>';
 
-    require_once('PHPMailer/PHPMailerAutoload.php');
+    require_once('../../PHPMailer/PHPMailerAutoload.php');
 
 
     $mail = new PHPMailer();
     $mail->isSMTP();
-    $mail->SMTPAuth;
+    $mail->SMTPAuth = true;
     $mail->SMTPSecure = 'ssl';
     $mail->Host = 'smtp.gmail.com';
     $mail->Port = '465';
