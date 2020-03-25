2d53bf988c06ee282d198e4e7882d798609ed1b9
Braden Scheidecker <53822929+bradenschei@users.noreply.github.com>
2020-03-25 06:48:43 -0500

    .

---
+++ Image-Upload/img-upload.php
@@ -0,0 +1,19 @@
+<?php include '../inc/nav.php'?>
+
+<br>
+<br>
+<br>
+<br>
+
+<form action="upload.php" method="POST" enctype="multipart/form-data">
+    <input type="file" name="file">
+    <button type="submit" name="submit">UPLOAD</button>
+</form>
+
+<br>
+<br>
+<br>
+<br>
+
+
+<?php include '../inc/footer.php'?>
