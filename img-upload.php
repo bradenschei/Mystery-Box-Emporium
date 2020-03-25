d5314c3736573a7bbf63f0207b430b5bfb1101ad
Braden Scheidecker <53822929+bradenschei@users.noreply.github.com>
2020-03-25 06:55:26 -0500

    .

--- img-upload.php
+++ img-upload.php
@@ -4,8 +4,10 @@
 <br>
 <br>
 <br>
+<br>
+<br>
 
-<form action="upload.php" method="POST" enctype="multipart/form-data">
+<form action="./inc/upload.php" method="POST" enctype="multipart/form-data">
     <input type="file" name="file">
     <button type="submit" name="submit">UPLOAD</button>
 </form>
